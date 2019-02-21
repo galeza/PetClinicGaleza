7766a66cef2bc092d6ef2e8817e754246fa8c4e6
galeza <galeza.agnieszka@gmail.com>
2019-02-21 21:34:10 +0100

    Add Owner pojo

--- src/main/java/com/galeza/petclinic/pojo/Owner.java
+++ src/main/java/com/galeza/petclinic/pojo/Owner.java
@@ -3,6 +3,12 @@ package com.galeza.petclinic.pojo;
 import lombok.Getter;
 import lombok.Setter;
 
+@Setter
+@Getter
 public class Owner {
 
+	private String name;
+	private String address;
+	private String city;
+	private String telephone;
 }
