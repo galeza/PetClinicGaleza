27bb9d056df96c62a526b11163837a2e6e4b6010
galeza <galeza.agnieszka@gmail.com>
2019-03-15 21:56:08 +0100

    Uncomment tests

--- src/test/java/com/galeza/petclinic/tests/PetClinicTest.java
+++ src/test/java/com/galeza/petclinic/tests/PetClinicTest.java
@@ -33,53 +33,53 @@ import javax.lang.model.element.Element;
 //@Listeners(TestListener.class)
 public class PetClinicTest extends BaseTest {
 
-//	@DataProvider(name = "ownerInformation")
-//
-//	public static Object[][] ownerInformation() {
-//
-//		return new Object[][] {
-//				{ "Jean", "Coleman", "ModifiedJean", "ModifiedColeman", "Kijowska", "Krakow", "123456789" }, };
-//
-//	}
-//
-//	@Test(dataProvider = "ownerInformation")
-//	public void modifyOwnerTest(String firstName, String lastName, String modifiedFirstName, String modifiedLastName,
-//			String address, String city, String telephone) {
-//		Home homePage = new Home(driver);
-//		OwnerInformation ownerInfoPage = homePage.open().goToFindOwners().showAllOwners()
-//				.goToOwner(firstName + " " + lastName);
-//		Owner existingOwner = ownerInfoPage.readInformationAboutOwner();
-//		assertThat(existingOwner.getFirstName()).isEqualToIgnoringCase(firstName);
-//		assertThat(existingOwner.getLastName()).isEqualToIgnoringCase(lastName);
-//		NewOwner newOwnerPage = ownerInfoPage.openNewOwnerPage();
-//		Owner modifiedOwner = setOwnerInformation(modifiedFirstName, modifiedLastName, address, city, telephone);
-//		newOwnerPage.updateOwnerInformation(modifiedOwner);
-//		ownerInfoPage = newOwnerPage.updateOwner();
-//		Owner readModifiedOwner = ownerInfoPage.readInformationAboutOwner();
-//		assertThat(readModifiedOwner).isEqualToComparingFieldByField(modifiedOwner);
-//	}
-//
-//	@DataProvider(name = "findSpecificOwner")
-//
-//	public static Object[][] findSpecificOwner() {
-//
-//		return new Object[][] {
-//				{ "Peter", "McTavish", "2387 S. Fair Way", "Madison", "6085552765" }, };
-//
-//	}
-//
-//	@Test(dataProvider = "findSpecificOwner")
-//	public void showSpecificOwnerTest(String firstName, String lastName,
-//			String address, String city, String telephone) {
-//		Home homePage = new Home(driver);
-//		OwnerInformation ownerInfoPage = homePage.open().goToFindOwners().showSpecificOwner(lastName);
-//		Owner foundOwner = ownerInfoPage.readInformationAboutOwner();
-//		assertThat(foundOwner.getFirstName()).isEqualToIgnoringCase(firstName);
-//		assertThat(foundOwner.getLastName()).isEqualToIgnoringCase(lastName);
-//		assertThat(foundOwner.getAddress()).isEqualToIgnoringCase(address);
-//		assertThat(foundOwner.getCity()).isEqualToIgnoringCase(city);
-//		assertThat(foundOwner.getTelephone()).isEqualToIgnoringCase(telephone);
-//	}
+	@DataProvider(name = "ownerInformation")
+
+	public static Object[][] ownerInformation() {
+
+		return new Object[][] {
+				{ "Jean", "Coleman", "ModifiedJean", "ModifiedColeman", "Kijowska", "Krakow", "123456789" }, };
+
+	}
+
+	@Test(dataProvider = "ownerInformation")
+	public void modifyOwnerTest(String firstName, String lastName, String modifiedFirstName, String modifiedLastName,
+			String address, String city, String telephone) {
+		Home homePage = new Home(driver);
+		OwnerInformation ownerInfoPage = homePage.open().goToFindOwners().showAllOwners()
+				.goToOwner(firstName + " " + lastName);
+		Owner existingOwner = ownerInfoPage.readInformationAboutOwner();
+		assertThat(existingOwner.getFirstName()).isEqualToIgnoringCase(firstName);
+		assertThat(existingOwner.getLastName()).isEqualToIgnoringCase(lastName);
+		NewOwner newOwnerPage = ownerInfoPage.openNewOwnerPage();
+		Owner modifiedOwner = setOwnerInformation(modifiedFirstName, modifiedLastName, address, city, telephone);
+		newOwnerPage.updateOwnerInformation(modifiedOwner);
+		ownerInfoPage = newOwnerPage.updateOwner();
+		Owner readModifiedOwner = ownerInfoPage.readInformationAboutOwner();
+		assertThat(readModifiedOwner).isEqualToComparingFieldByField(modifiedOwner);
+	}
+
+	@DataProvider(name = "findSpecificOwner")
+
+	public static Object[][] findSpecificOwner() {
+
+		return new Object[][] {
+				{ "Peter", "McTavish", "2387 S. Fair Way", "Madison", "6085552765" }, };
+
+	}
+
+	@Test(dataProvider = "findSpecificOwner")
+	public void showSpecificOwnerTest(String firstName, String lastName,
+			String address, String city, String telephone) {
+		Home homePage = new Home(driver);
+		OwnerInformation ownerInfoPage = homePage.open().goToFindOwners().showSpecificOwner(lastName);
+		Owner foundOwner = ownerInfoPage.readInformationAboutOwner();
+		assertThat(foundOwner.getFirstName()).isEqualToIgnoringCase(firstName);
+		assertThat(foundOwner.getLastName()).isEqualToIgnoringCase(lastName);
+		assertThat(foundOwner.getAddress()).isEqualToIgnoringCase(address);
+		assertThat(foundOwner.getCity()).isEqualToIgnoringCase(city);
+		assertThat(foundOwner.getTelephone()).isEqualToIgnoringCase(telephone);
+	}
 
 	@DataProvider(name = "ownerAddPet")
 
